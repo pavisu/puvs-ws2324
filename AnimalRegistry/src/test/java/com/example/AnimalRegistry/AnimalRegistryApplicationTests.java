e9e4d74dc6d912a0cfc03af4d0db9dac4c6e3e87
Pavithra Sureshkumar <146721357+pavisu@users.noreply.github.com>
2023-10-17 19:35:51 +0000

    Add unzipped AnimalRegistry file

---
+++ AnimalRegistry/src/test/java/com/example/AnimalRegistry/AnimalRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnimalRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
