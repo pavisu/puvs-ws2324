ced286672cad8be023ffe968ca39d5a4f9d718c1
pavisu <pasuit00@outlook.com>
2023-11-01 17:54:10 +0100

    Comment Animal AnimalRepo AnimalServices

--- AnimalRegistry/src/main/java/com/example/AnimalRegistry/Entity/Animal.java
+++ AnimalRegistry/src/main/java/com/example/AnimalRegistry/Entity/Animal.java
@@ -3,12 +3,31 @@ package com.example.AnimalRegistry.Entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+// This class is annotated with @Document, indicating that it's a MongoDB document 
+// and specifying the collection name as "animals."
 @Document(collection = "animals")
 public class Animal {
 
+    // The @Id annotation marks the "animalid" field as the primary key in the MongoDB document.
     @Id
     private String animalid;
-    
+
+    // This is a constructor that allows you to create an instance of the "Animal" class with values for all fields.
+
+    public Animal(String animalid, String animalname, String animaladdress, String phonenumber) {
+        this.animalid = animalid;
+        this.animalname = animalname;
+        this.animaladdress = animaladdress;
+        this.phonenumber = phonenumber;
+    }
+
+     public Animal() {
+        // This is a default no-argument constructor, which is required for some frameworks and libraries. It initializes an instance of "Animal" with default values.
+    }
+
+
+    // Getters and setters are provided for the "animalid" field to access and modify its value.
+
     public String getAnimalid() {
         return animalid;
     }
@@ -17,6 +36,8 @@ public class Animal {
         this.animalid = animalid;
     }
 
+
+    // Getters and setters are provided for the "animalname" field.
 
     private String animalname;
     
@@ -29,6 +50,8 @@ public class Animal {
     }
 
 
+    // Getters and setters are provided for the "animaladdress" field.
+
     private String animaladdress;
     
     public String getAnimaladdress() {
@@ -40,6 +63,8 @@ public class Animal {
     }
 
 
+    // Getters and setters are provided for the "phonenumber" field.
+
     private String phonenumber;
 
     public String getPhonenumber() {
@@ -50,15 +75,8 @@ public class Animal {
         this.phonenumber = phonenumber;
     }
 
-    public Animal(String animalid, String animalname, String animaladdress, String phonenumber) {
-        this.animalid = animalid;
-        this.animalname = animalname;
-        this.animaladdress = animaladdress;
-        this.phonenumber = phonenumber;
-    }
-
-    public Animal() {}
-
+    
+    // The toString() method is overridden to provide a custom string representation of the "Animal" object.
    @Override
     public String toString(){
         return "Animal{" +
--- AnimalRegistry/src/main/java/com/example/AnimalRegistry/Service/AnimalServices.java
+++ AnimalRegistry/src/main/java/com/example/AnimalRegistry/Service/AnimalServices.java
@@ -7,27 +7,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+// This class is marked with the @Service annotation, indicating that it's a Spring service component, 
+// responsible for providing business logic related to animals.
 @Service
 public class AnimalServices {
     
+    // This field is annotated with @Autowired, indicating that Spring should inject an instance of the AnimalRepo repository into this field. 
+    // The repository is used for database interactions with animal data.
     @Autowired
     private AnimalRepo repo;
 
+    // This method allows saving or updating an animal record in the database.
     public void saveorUpdate(Animal animals){
 
+        // It calls the "save" method on the "repo" (AnimalRepo) to save the provided "animals" object to the database.
         repo.save(animals);
 
     }
 
+    // This method retrieves a list of all animal records from the database.
     public Iterable<Animal> listAll() {
+                
+        // It calls the "findAll" method on the "repo" to fetch all animal records and returns them as an iterable collection.
         return repo.findAll();
     }
 
+    // This method allows deleting an animal record by its ID from the database.
     public void deleteAnimal(String id) {
+
+        // It calls the "deleteById" method on the "repo" and provides the "id" parameter to delete the animal with the specified ID.
         repo.deleteById(id);
     }
 
+    // This method retrieves an animal record by its ID from the database.
     public Animal getAnimalById(String animalid) {
+
+        // It calls the "findById" method on the "repo" and provides the "animalid" parameter. 
+        // The ".get()" method is used to extract and return the found animal object.
         return repo.findById(animalid).get();
     }
 
