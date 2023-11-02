e7a8561fab9eb0c29a8bc61bc08412866df3cd1b
pavisu <pasuit00@outlook.com>
2023-11-02 15:43:31 +0100

    Implement Jaeger and Otel

--- AnimalRegistry/src/main/java/com/example/AnimalRegistry/AnimalRegistryApplication.java
+++ AnimalRegistry/src/main/java/com/example/AnimalRegistry/AnimalRegistryApplication.java
@@ -3,10 +3,7 @@ package com.example.AnimalRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.Bean;
 
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 
 
 
@@ -16,11 +13,6 @@ public class AnimalRegistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimalRegistryApplication.class, args);
-	}
-
-	@Bean
-	public OpenTelemetry openTelemetry() {
-	  return AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
 	}
     
 
