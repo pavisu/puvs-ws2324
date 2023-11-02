a0ba8a69d4a46c618dc7a14800d89e14ae2dcce7
pavisu <pasuit00@outlook.com>
2023-11-03 00:13:24 +0100

    Trying to run jaeger

--- AnimalRegistry/src/main/java/com/example/AnimalRegistry/AnimalRegistryApplication.java
+++ AnimalRegistry/src/main/java/com/example/AnimalRegistry/AnimalRegistryApplication.java
@@ -2,11 +2,12 @@ package com.example.AnimalRegistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
-
-
-
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.TracerProvider;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
 
 @SpringBootApplication
 public class AnimalRegistryApplication {
@@ -14,14 +15,31 @@ public class AnimalRegistryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AnimalRegistryApplication.class, args);
 	}
-    
 
-	/*@RestController
-	class Greet {
-		@RequestMapping("/")
-		String greeting() {
-			return "Hello Cat!";
+	@Configuration
+	public class OpenTelemetryConfig {
+
+		@Bean
+		public Tracer jaegerTracer() {
+			// Create a TracerProvider using OpenTelemetrySdk
+			TracerProvider tracerProvider = SdkTracerProvider.builder().build();
+
+			// Get a Tracer instance from the TracerProvider
+			Tracer tracer = tracerProvider.get("jaeger-service");
+
+			// Return the Tracer
+			return tracer;
 		}
-	}*/
-}
+	}
 
+	/*
+	 * @RestController
+	 * class Greet {
+	 * 
+	 * @RequestMapping("/")
+	 * String greeting() {
+	 * return "Hello Cat!";
+	 * }
+	 * }
+	 */
+}
